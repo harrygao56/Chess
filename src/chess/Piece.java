0249dc5bc7cb726516da102ca0f277520de5d4d5
harrygao56 <harrygao56@gmail.com>
2022-01-02 18:40:08 -0600

    Piece

--- src/chess/Piece.java
+++ src/chess/Piece.java
@@ -1,5 +1,29 @@
 package chess;
 
 public abstract class Piece {
-
+	private boolean alive;
+	private boolean white;
+	
+	public Piece(boolean white)
+	{
+		this.white = white;
+		alive = true;
+	}
+	
+	public boolean isAlive()
+	{
+		return alive;
+	}
+	
+	public boolean isWhite()
+	{
+		return white;
+	}
+	
+	public void die()
+	{
+		alive = false;
+	}
+	
+	public abstract void moves();
 }
